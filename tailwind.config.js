c2a6ac702173be96e6da0940ae7f2bcf2fd786f5
josevi97 <josevi00@hotmail.com>
2025-06-21 17:10:57 +0200

    chore: adds tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust to your structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
